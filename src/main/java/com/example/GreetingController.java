4d1349975db1ea132613b325225a8b52c8b5ba41
terradistac <terradistacc@gmail.com>
2023-06-21 14:48:43 -0400

    Changing the @GetMapping to @RequestMapping

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,7 +9,7 @@ public class GreetingController {
 
 	private static final String template = "Hello, World! ";
 
-	@GetMapping("/greeting")
+	@RequestMapping("/greeting")
 	public String greeting() {
 		return template;
 	}
